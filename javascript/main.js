23dfc0b2c9297a4f4b8bda86839e947663ad42e5
LeoHallakAlvarez <detroitborg92@hotmail.com>
2024-01-13 00:56:04 -0300

    vamos

---
+++ javascript/main.js
@@ -0,0 +1,4 @@
+alert("Bienvenidos a la VinoTeka")
+prompt("Para ingresar al sitio debe ser mayor de edad, tiene mas de 18? Ingrese su edad a continuación")
+prompt("Confirma bajo su consentimiento que es mayor de edad? Presione aceptar si es correcto")
+alert("ACLARACIÓN! La empresa no se hace responsable por futuros inconvenientes si ust no es mayor de 18 años!")
